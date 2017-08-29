4a3ec99d2960bb61d9f84c7fbf7c6923cb8b5142
mageshnagarajan95 <30798706+mageshnagarajan95@users.noreply.github.com>
2017-08-29 13:07:38 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -68,12 +68,12 @@ app.get('/', function (req, res) {
 });
 
 function hash(input, salt){
-    var hashed=crypto.pbkdf2Sync(input, salt, 10000, 512, 'she512');
+    var hashed=crypto.pbkdf2Sync(input, salt, 10000, 512, 'sha512');
 
     return hashed.toString('hex');
 }
 app.get('/hash/:input',function(req,res){
-    var hashedString=hash(req.params.input, "this-is-some-random");
+    var hashedString=hash(req.params.input, 'this-is-some-random');
     res.send(hashedString);
 });
 app.get('/ui/style.css', function (req, res) {
