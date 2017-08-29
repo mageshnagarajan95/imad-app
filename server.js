90cda638059adaf88dc5499e9977620594a714f2
mageshnagarajan95 <30798706+mageshnagarajan95@users.noreply.github.com>
2017-08-29 12:42:30 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 var express = require('express');
 var morgan = require('morgan');
 var path = require('path');
-
+var crypto=require('crypto');
 var app = express();
 app.use(morgan('combined'));
 var articles ={
@@ -66,7 +66,15 @@ return htmlTemplate;
 app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
+function hash(input,salt){
+    var hashed=crypto.pbkdf2Sync(input, salt, 10000,512,'she512');
 
+    return hashed.toString('hex');
+}
+app.get('/hash/input',function(req,res){
+    var hashedString=hash(req.param.input,"this-is-some-random");
+    res.send(hashedString);
+});
 app.get('/ui/style.css', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'style.css'));
 });
